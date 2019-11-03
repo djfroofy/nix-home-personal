f828abdb614b275c3872be58d41489f4bb5c2341
Drew Smathers <drew.smathers@gmail.com>
2019-11-02 23:16:15 -0700

    added more packages

--- packages.nix
+++ packages.nix
@@ -1,4 +1,112 @@
 pkgs: with pkgs; [
+  # X related stuff
+  xmobar
+  arandr
+  haskellPackages.xmonad-wallpaper
+  feh
+  firefox
+  # google-chrome
+  acpi
+  rofi
+  adobe-reader
+  aesop
+  freerdp
+  usbutils
+  
+  # Keyboard stuff
+  xsel
+  xclip
+  dmenu
+  maim
+  xdotool
+  slop
+  ffmpeg
+
+  # Screen, Mouse sharing
+  # synergy
+
+  # Chatting, conferencing, emailing
+  #mutt
+  fetchmail
+  zoom-us
+  slack
+  procmail
+  libreoffice
+  w3m-full
+  #haskellPackages.attomail
+  #mailcap
+  #postfix
+
+  # slack-term
+
+  # Health and Well-being
+  redshift
+
+  # ---
+  # Development
+  # ----
+
+  # General
+  gnumake
+  cmake
+  direnv
+  minicom
+  broot
+  neofetch
+  dnsutils
+  i7z
+  lm_sensors
+
+  # Java
+  maven
+  oraclejdk8
+
+  # C
+  gcc
+
+  # Python and python packages
+  python
+  python27Packages.virtualenv
+  python37
+  python37Packages.virtualenv
+  python37Packages.glances
+
+  # code review stuff
+  arcanist
+
+  # Ruby
+  ruby
+
+  # Go
+  go
+  protobuf
+
+  # Haskell
+  ghc
+
+  # Code editors, IDEs 
+  vscode
+
+  # Rust
+  rustc
+  rustPlatform.rustcSrc
+  cargo
+  carnix
+
+  # Shells, remote debugging 
+  mosh
+
+  # Misc others
+  mimic
+  rpm
+  fpm
+  fceux
+  nixops
+  radeontop
+  go-md2man
+
+  unrar
+  xorg.xdpyinfo
   # ---
   # AV and Games
   # ---
@@ -53,4 +161,5 @@ pkgs: with pkgs; [
   arduino
   fritzing
   ino
+
 ]

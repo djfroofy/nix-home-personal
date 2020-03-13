d7ac1c39913964fe63bc8ed2c66af37ba353f855
Drew Smathers <drew.smathers@oracle.com>
2020-03-13 13:43:11 -0700

    temporary: comment out spotifd service config

--- home.nix
+++ home.nix
@@ -5,14 +5,15 @@
   programs.git.signing.key = "85E0B175FAE8B88F";
   programs.git.signing.signByDefault = true;
 
-  services.spotifyd = {
-    enable = true;
-    settings = {
-      global = {
-        username = "djfroofy";
-        password_cmd = "${pkgs.pass}/bin/pass spotify/djfroofy";
-        on_song_change_hook = "/home/dsmather/bin/spotifyd-song-changed";
-      };
-    };
-  };
+
+#  services.spotifyd = {
+#    enable = true;
+#    settings = {
+#      global = {
+#        username = "djfroofy";
+#        password_cmd = "${pkgs.pass}/bin/pass spotify/djfroofy";
+#        on_song_change_hook = "/home/dsmather/bin/spotifyd-song-changed";
+#      };
+#    };
+#  };
 }

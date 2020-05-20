47fd80ddfb1334f41f6c3ef60e3713bffaf20388
Drew Smathers <drew.smathers@oracle.com>
2020-05-20 16:04:02 -0700

    Add printing configurations to be imported from configuration.nix

---
+++ printing.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+
+  services.printing = {
+    enable = true;
+    # browsing = true;
+    # defaultShared = true;
+    drivers = with pkgs; [
+      brlaser
+      brgenml1lpr
+      brgenml1cupswrapper
+      hll2390dw-cups
+    ];
+  };
+
+  hardware.printers.ensurePrinters = [{
+    name = "Brother_HLL2390DW";
+    description = "Brother HLL2390DW";
+    location = "Home Office Printer";
+    deviceUri = "ipp://192.168.1.154/ipp/port1";
+    model = "brother-HLL2390DW-cups-en.ppd";
+  }];
+}

65af7c892e467203a5e11a2b114cb5d1345d53f7
Drew Smathers <drew.smathers@oracle.com>
2020-05-20 22:13:52 -0700

    separate personal and work printing stations

---
+++ printers.nix
@@ -0,0 +1,7 @@
+[{
+  name = "Brother_HLL2390DW";
+  description = "Brother HLL2390DW";
+  location = "Home Office";
+  deviceUri = "ipp://192.168.1.154/ipp/port1";
+  model = "brother-HLL2390DW-cups-en.ppd";
+}]
--- printing.nix
+++ printing.nix
@@ -14,11 +14,8 @@
     ];
   };
 
-  hardware.printers.ensurePrinters = [{
-    name = "Brother_HLL2390DW";
-    description = "Brother HLL2390DW";
-    location = "Home Office";
-    deviceUri = "ipp://192.168.1.154/ipp/port1";
-    model = "brother-HLL2390DW-cups-en.ppd";
-  }];
+  hardware.printers.ensurePrinters =
+    (import ./printers.nix)
+    ++ (import ../work/printers.nix);
+
 }
